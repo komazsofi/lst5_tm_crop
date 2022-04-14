f19c7ea2f72092cf1a5fb8741115c48fc098f55f
Zsofia Viktoria Koma <au700510@uni.au.dk>
2022-04-14 15:23:44 +0200

    apply workflow whole study area

--- deeplearning/Step2_prepmask_fromcorine.R
+++ deeplearning/Step2_prepmask_fromcorine.R
@@ -1,18 +1,18 @@
 library(terra)
 
 # Set working directory
-workingdirectory="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/landsat5_tm_trial/"
+workingdirectory="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/landsat5_tm_trial3/"
 setwd(workingdirectory)
 
 corine_file_1990="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/field_data/corine_1990_epgs25832.shp"
-smallstudyarea_file="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/landsat5_tm_trial/split_15km/img_31.tif"
+studyarea_file="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/landsat5_tm_trial3/LTM5_ndvi_coll.tif"
 
 # Import & clip to extent
 
 corine=vect(corine_file_1990)
-smallstudyarea=rast(smallstudyarea_file)
+studyarea=rast(studyarea_file)
 
-corine_crop=terra:::crop(corine, smallstudyarea)
+corine_crop=terra:::crop(corine, studyarea)
 
 # Create mask (only agriculture 1 others are 0)
 
@@ -21,8 +21,8 @@ corine_crop[corine_crop$code_90==211] <- 1
 
 # Rasterize
 
-corine_crop_rasterized <- terra:::rasterize(corine_crop, smallstudyarea,field="code_90",fun=max)
+corine_crop_rasterized <- terra:::rasterize(corine_crop, studyarea,field="code_90",fun=max)
 
 # Export
-terra:::writeRaster(corine_crop_rasterized,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/landsat5_tm_trial/split_15km/img_31_mask.tif")
+terra:::writeRaster(corine_crop_rasterized,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/landsat5_tm_trial3/LTM5_ndvi_coll_mask.tif")
   
--- deeplearning/Step2b_makealignestiles.R
+++ deeplearning/Step2b_makealignestiles.R
@@ -1,11 +1,11 @@
 library(terra)
 
 # Set working directory
-workingdirectory="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/landsat5_tm_trial2/"
+workingdirectory="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/landsat5_tm_trial3/"
 setwd(workingdirectory)
 
-studyarea_file="img_31.tif"
-studyarea_file_mask="img_31_mask.tif"
+studyarea_file="LTM5_ndvi_coll.tif"
+studyarea_file_mask="LTM5_ndvi_coll_mask.tif"
 
 # Import & clip to extent
 
@@ -16,6 +16,13 @@ studyarea_mask=rast(studyarea_file_mask)
 
 # Make tiles
 
+dir.create("img")
+dir.create("mask")
+
 studyarea_300m <- aggregate(studyarea, fact=112)
+
+setwd("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/landsat5_tm_trial3/img/")
 studyarea_300m_tiles <- makeTiles(studyarea, studyarea_300m)
+
+setwd("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/landsat5_tm_trial3/mask/")
 studyarea_300m_tiles_mask <- makeTiles(studyarea_mask, studyarea_300m)
